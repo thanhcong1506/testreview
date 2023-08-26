2bdb45927e642818144a58910bc7cbf2a1a7d598
thanhcong <thanhcong1506@gmail.com>
2023-08-26 15:10:39 +0700

    fix mobile menu

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const menuBar = document.querySelector("#menu-bar");
 const mobileMenu = document.querySelector("#mobile-menu");
+console.log(mobileMenu);
 
 menuBar.addEventListener("click", (e) => {
   mobileMenu.classList.toggle("hidden");
 });
+
+const menuItems = document.querySelectorAll("#mobile-menu li ");
+for (let i = 0; i < menuItems.length; i++) {
+  const menuItem = menuItems[i];
+  console.log(menuItems[i]);
+
+  menuItem.onclick = () => {
+    mobileMenu.classList.toggle("hidden");
+  };
+}
